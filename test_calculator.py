b88e366147050beff5bfef284fa92d7683163bed
WilliamWiegand <w.wiegand@my.bpp.com>
2025-07-17 14:33:47 +0100

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+import pytest
+from calculator import add, divide 
+
+def test_add_pass():
+    result = add(4, 10)
+    assert result == 14
+
+def test_add_fail():
+    result = add(4, 10)
+    assert result == 15
